a44915d1451b1432177faf31e1180e828d80a7be
Daniel <daniel.bartov@gmail.com>
2019-12-18 19:21:30 +0200

    asd

--- NODE/Test.java
+++ NODE/Test.java
@@ -2,14 +2,16 @@ package NODE;
 
 public class Test {
     public static void main(String[] args) {
-        Node<Object> chain = new Node<>(5);
-        Node<Object> temp = chain;
-        for (int i = 0; i < 10; i++) {
-            temp.setNext(new Node<>(i));
-            temp = temp.getNextNode();
-        }
-        temp.setNext(chain);
+        Node<Integer> meetingNode = new Node<Integer>(32);
+        Node<Integer> chain = new Node<Integer>(123,new Node<Integer>(
+                5,new Node<Integer>(7,new Node<Integer>(-65,meetingNode))));
+        Node<Integer> chain1 = new Node<Integer>(932,new Node<Integer>(
+                93,new Node<Integer>(75,new Node<Integer>(-81,meetingNode))));
+        meetingNode.setNext( new Node<Integer>(999,new Node<Integer>(
+                666,new Node<Integer>(5312,new Node<Integer>(-5123)))));
+        Utils.printChain(chain);
+        Utils.printChain(chain1);
+        
 
-        System.out.println(Utils.isCircled(chain));
     }
 }
--- NODE/Utils.java
+++ NODE/Utils.java
@@ -1,27 +1,25 @@
 package NODE;
 
 
-import java.util.function.ToDoubleBiFunction;
-
 public class Utils {
 
-    public static boolean isCircled(Node<?> chain){
+    public static boolean isCircled(Node<?> chain) {
         Node<?> pos1 = chain;
         Node<?> pos2 = chain;
-            while (pos2.hasNext() && pos2.getNextNode().hasNext()){
-                pos2 = pos2.getNextNode().getNextNode();
-                pos1 = pos1.getNextNode();
-                if (pos1 == pos2)
-                    return true;
-            }
+        while (pos2.hasNext() && pos2.getNextNode().hasNext()) {
+            pos2 = pos2.getNextNode().getNextNode();
+            pos1 = pos1.getNextNode();
+            if (pos1 == pos2)
+                return true;
+        }
         return false;
     }
 
-    private static <T> Node<T> findMeetingNode(Node<T> chain){
+    private static <T> Node<T> findMeetingNode(Node<T> chain) {
         Node<T> pos1 = chain;
         Node<T> pos2 = chain;
-        if(isCircled(chain)){
-            while (pos2.hasNext() && pos2.getNextNode().hasNext()){
+        if (isCircled(chain)) {
+            while (pos2.hasNext() && pos2.getNextNode().hasNext()) {
                 pos2 = pos2.getNextNode().getNextNode();
                 pos1 = pos1.getNextNode();
                 if (pos1 == pos2)
@@ -31,32 +29,21 @@ public class Utils {
         return null;
     }
 
-    private static int loopLength(Node<?> nodeInLoop){
-        Node<?> pos = nodeInLoop;
-        int counter = 1;
-        pos = pos.getNextNode();
-        while (pos != nodeInLoop){
-            counter++;
-            pos = pos.getNextNode();
-        }
-        return counter;
-    }
-
-    public static <T> Node<T> getStartLoop(Node<T> chain){
+    public static <T> Node<T> getStartLoop(Node<T> chain) {
         Node<T> meetingNode;
         Node<T> pos2 = chain;
-        if (isCircled(chain)){
+        if (isCircled(chain)) {
             meetingNode = findMeetingNode(chain);
             //TODO
         }
         return null;
     }
 
-    public static boolean isAlphaBet(Node<String> chain){
+    public static boolean isAlphaBet(Node<String> chain) {
 
-        while (chain.hasNext()){
+        while (chain.hasNext()) {
             Node<String> pos = chain;
-            while (pos.hasNext()){
+            while (pos.hasNext()) {
                 pos = pos.getNextNode();
                 if (chain.getValue().compareTo(pos.getValue()) > 0)
                     return false;
@@ -66,10 +53,10 @@ public class Utils {
         return true;
     }
 
-    public static <T extends Comparable<T>> T maxValueInChan(Node<T> chain){
+    public static <T extends Comparable<T>> T maxValueInChan(Node<T> chain) {
         T max = chain.getValue();
         chain = chain.getNextNode();
-        while (chain.hasNext()){
+        while (chain.hasNext()) {
             max = max.compareTo(chain.getValue()) >= 0 ? max : chain.getValue();
             chain = chain.getNextNode();
         }
@@ -95,20 +82,84 @@ public class Utils {
         return min;
     }
 
-    public static Node<Integer> getIntChain(int n){
+    public static Node<Integer> getIntChain(int n) {
         Node<Integer> chain = new Node<Integer>(n);
-        for (int i = n; i > 1  ; i--){
-            chain = new Node<Integer>(n-1, chain);
+        for (int i = n; i > 1; i--) {
+            chain = new Node<Integer>(n - 1, chain);
         }
         return chain;
     }
 
-    public static <T> void printChain(Node<T> chain){
-        while (chain.hasNext()){
-            System.out.print(chain.getValue() + ", ");
+    public static <T> void printChain(Node<T> chain) {
+        while (chain.hasNext()) {
+            System.out.print(chain.getValue() + "--> ");
             chain = chain.getNextNode();
         }
         System.out.println(chain.getValue());
     }
+
+    //chain is not circled
+    public static int chainLength(Node<?> chain) {
+        if (isCircled(chain))
+            return -1;
+        int counter = 0;
+        while (chain.hasNext()) {
+            counter++;
+            chain = chain.getNextNode();
+        }
+        return counter;
+    }
+
+    //both chains should not be circled
+    private static <T> Node<T> findMeetingNode(Node<T> chain1, Node<T> chain2) {
+        int difference = Math.abs(chainLength(chain1) - chainLength(chain2));
+
+        if (chainLength(chain1) > chainLength(chain2)) {
+            for (int i = 0; i < difference; i++) {
+                chain1 = chain1.getNextNode();
+            }
+        } else {
+            for (int i = 0; i < difference; i++) {
+                chain2 = chain2.getNextNode();
+            }
+        }
+
+        while (chain1 != chain2) {
+            if (chain1.hasNext() && chain2.hasNext()) {
+                chain1 = chain1.getNextNode();
+                chain2 = chain2.getNextNode();
+            } else {
+                return null;
+            }
+        }
+        return chain1;
+    }
+
+    private static <T> Node<T> findNodeBefore(Node<T> chain, Node<T> node) {
+        while (chain.hasNext()) {
+            if (chain.getNextNode() == node)
+                return chain;
+            chain = chain.getNextNode();
+        }
+        System.out.println("The chain never reaches the node");
+        return null;
+    }
+
+    public <T> void breakChains(Node<T> chain1, Node<T> chain2) {
+        Node<T> meetingNode = findMeetingNode(chain1, chain2);
+        Node<T> beforeMeetingNode1 = findNodeBefore(chain1, meetingNode);
+        Node<T> beforeMeetingNode2 = findNodeBefore(chain2, meetingNode);
+
+        if (beforeMeetingNode1 != null && beforeMeetingNode2 != null) {
+            while (beforeMeetingNode1.hasNext() && beforeMeetingNode2.hasNext()) {
+                beforeMeetingNode1.setNext(new Node<T>(beforeMeetingNode1.getNextNode().getValue(), beforeMeetingNode1.getNextNode().getNextNode()));
+                beforeMeetingNode2.setNext(new Node<T>(beforeMeetingNode2.getNextNode().getValue(), beforeMeetingNode2.getNextNode().getNextNode()));
+                beforeMeetingNode1 = beforeMeetingNode1.getNextNode();
+                beforeMeetingNode2 = beforeMeetingNode2.getNextNode();
+            }
+        }
+    }
+
+
 }
 
